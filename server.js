3e891e3017da3f278232d615f63675166cb9c333
Nisarat Saelim <nisaratnsaelim@gmail.com>
2018-12-15 13:28:37 +0700

    add

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var db = require('./database');
+//database.js สามารถดึงข้อมูลทั้งหมดมาใช้ได้
+var cors = require('cors');
+app.use(cors())
+
+var bodyParser = require('body-parser');
+//ตัวนี้สำคัญ
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+
+//add routing
+//index page
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+console.log('App is running on http://localhost:' + port);
+});
